23912b728c4d0f4f0d1af5e1d597c2af4c238a35
AcrylicShrimp <led789zxpp@naver.com>
2020-06-08 22:47:26 +0900

    FEAT: Add a attribute.

---
+++ src/core/attribute.rs
@@ -0,0 +1,56 @@
+extern crate serde;
+
+use super::value::{Value, ValueType};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd, Eq, Ord)]
+pub struct Attribute {
+	name: String,
+	desc: Option<String>,
+	value_type: ValueType,
+	default_value: Option<Value>,
+}
+
+impl Attribute {
+	pub fn new(
+		name: String,
+		desc: Option<String>,
+		value_type: ValueType,
+		default_value: Option<Value>,
+	) -> Attribute {
+		match &default_value {
+			Some(default_value) => {
+				if default_value.get_type() != value_type {
+					panic!(
+						"default value {:?} is not {:?} type",
+						default_value, value_type
+					);
+				}
+			}
+			None => {}
+		}
+
+		Attribute {
+			name,
+			desc,
+			value_type,
+			default_value,
+		}
+	}
+
+	pub fn name(&self) -> &String {
+		&self.name
+	}
+
+	pub fn desc(&self) -> Option<&String> {
+		self.desc.as_ref()
+	}
+
+	pub fn value_type(&self) -> &ValueType {
+		&self.value_type
+	}
+
+	pub fn default_value(&self) -> Option<&Value> {
+		self.default_value.as_ref()
+	}
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,2 +1,3 @@
+pub mod attribute;
 pub mod task;
 pub mod value;
