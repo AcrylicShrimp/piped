a3735927d542e3aee5f26413d7bb0b714cc8a188
AcrylicShrimp <led789zxpp@naver.com>
2020-06-08 22:48:37 +0900

    TEST: Add some test codes.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod core;
 
 fn main() {
-    let task = core::task::Task::new("my-task".to_owned());
-    println!("{}", task.name());
+    let mut task = core::task::Task::new(
+        "copy".to_owned(),
+        Some("copy the src file to dst".to_owned()),
+    );
+
+    task.add_attributes(core::attribute::Attribute::new(
+        "src".to_owned(),
+        Some("a source file to be copied".to_owned()),
+        core::value::ValueType::Str,
+        None,
+    ));
+    task.add_attributes(core::attribute::Attribute::new(
+        "dst".to_owned(),
+        Some("a destination path for the copied file to be placed".to_owned()),
+        core::value::ValueType::Str,
+        None,
+    ));
+
+    println!("{:?}", task);
 }
