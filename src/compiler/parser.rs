8e7871c1ac29e3fe4d9251cf412b2c53dc4ffe1e
AcrylicShrimp <led789zxpp@naver.com>
2020-07-14 09:15:56 +0900

    FEAT: Add support for set statements.

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1,19 +1,35 @@
 use super::lexer::{Lexer, Token, TokenType};
 
+#[derive(Debug)]
 pub enum AST {
     Set(SetAST),
+    Print(SetAST),
+    PrintErr(SetAST),
 }
 
+#[derive(Debug)]
 pub struct SetAST {
-    pub name: String,
+    pub name: Token,
     pub value: ExpressionAST,
 }
 
+#[derive(Debug)]
+pub struct PrintAST {
+    pub expressionVec: Vec<ExpressionAST>,
+}
+
+#[derive(Debug)]
+pub struct PrintErrAST {
+    pub expressionVec: Vec<ExpressionAST>,
+}
+
+#[derive(Debug)]
 pub enum ExpressionAST {
     Literal(LiteralAST),
     Variable(Token),
 }
 
+#[derive(Debug)]
 pub enum LiteralAST {
     Bool(Token),
     Integer(Token),
@@ -22,13 +38,31 @@ pub enum LiteralAST {
 
 enum ParserStatus {
     TopLevel,
-    Statement { atToken: Token },
-    Pipeline { nameToken: Token },
+    Statement,
+    StatementSet,
+    Pipeline {
+        nameToken: Token,
+    },
+    PipelineArgument {
+        nameToken: Token,
+        arguemntTokenVec: Vec<Token>,
+    },
 }
 
-fn parse(lexer: &mut Lexer) -> Vec<AST> {
+pub fn parse(lexer: &mut Lexer) -> Vec<AST> {
+    fn next(lexer: &mut Lexer) -> Token {
+        loop {
+            let token = lexer.next();
+
+            if token.token_type == TokenType::Comment {
+                continue;
+            }
+
+            return token;
+        }
+    }
     fn next_token(lexer: &mut Lexer, token_type: TokenType) -> Token {
-        let token = lexer.next();
+        let token = next(lexer);
 
         if token.token_type != token_type {
             panic!(
@@ -45,14 +79,14 @@ fn parse(lexer: &mut Lexer) -> Vec<AST> {
 
     'parse: loop {
         if let ParserStatus::TopLevel = &status {
-            let token = lexer.next();
+            let token = next(lexer);
 
             if token.token_type == TokenType::Eof {
                 return ast_vec;
             }
 
             if token.token_type == TokenType::At {
-                status = ParserStatus::Statement { atToken: token };
+                status = ParserStatus::Statement;
                 continue 'parse;
             }
 
@@ -62,17 +96,60 @@ fn parse(lexer: &mut Lexer) -> Vec<AST> {
             }
 
             panic!("unexpected token \"{:#?}\" found", token);
-        } else if let ParserStatus::Statement { atToken } = &status {
-            let token = lexer.next();
+        } else if let ParserStatus::Statement = &status {
+            let statementToken = next(lexer);
 
-            match token.token_type {
+            match statementToken.token_type {
                 TokenType::KeywordSet => {
-                    let nameToken = next_token(lexer, TokenType::Id);
-                    next_token(lexer, TokenType::Equal);
+                    status = ParserStatus::StatementSet;
+                    continue 'parse;
                 }
-                _ => panic!("unexpected token \"{:#?}\" found", token),
+                _ => panic!("unexpected token \"{:#?}\" found", statementToken),
             }
+        } else if let ParserStatus::StatementSet = &status {
+            let nameToken = next_token(lexer, TokenType::Id);
+
+            next_token(lexer, TokenType::Equal);
+
+            let expressionToken = next(lexer);
+
+            if expressionToken.token_type != TokenType::LiteralBool
+                && expressionToken.token_type != TokenType::LiteralInteger
+                && expressionToken.token_type != TokenType::LiteralString
+                && expressionToken.token_type != TokenType::Id
+            {
+                panic!("unexpected token \"{:#?}\" found", expressionToken)
+            }
+
+            next_token(lexer, TokenType::Semicolon);
+
+            ast_vec.push(AST::Set {
+                0: SetAST {
+                    name: nameToken,
+                    value: match expressionToken.token_type {
+                        TokenType::LiteralBool => ExpressionAST::Literal {
+                            0: LiteralAST::Bool { 0: expressionToken },
+                        },
+                        TokenType::LiteralInteger => ExpressionAST::Literal {
+                            0: LiteralAST::Bool { 0: expressionToken },
+                        },
+                        TokenType::LiteralString => ExpressionAST::Literal {
+                            0: LiteralAST::Bool { 0: expressionToken },
+                        },
+                        TokenType::Id => ExpressionAST::Variable { 0: expressionToken },
+                        _ => unreachable!(),
+                    },
+                },
+            });
+
+            status = ParserStatus::TopLevel;
+            continue 'parse;
         } else if let ParserStatus::Pipeline { nameToken } = &status {
+        } else if let ParserStatus::PipelineArgument {
+            nameToken,
+            arguemntTokenVec,
+        } = &mut status
+        {
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 mod compiler;
 mod core;
 
-use compiler::lexer::{Lexer, TokenType};
+use compiler::lexer::Lexer;
+use compiler::parser::parse;
 use std::fs::read_to_string;
 
 fn main() {
     let mut lexer = Lexer::new(read_to_string("examples/basics.piped").unwrap());
+    let ast_vec = parse(&mut lexer);
 
-    loop {
-        let token = lexer.next();
-
-        if token.token_type == TokenType::Eof {
-            break;
-        }
-
-        println!("{:#?}", token);
-    }
+    println!("{:#?}", ast_vec);
 }
