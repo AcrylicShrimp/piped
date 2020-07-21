dc77e80b811fee163510e7c634ab4c30ed0e1fb4
AcrylicShrimp <led789zxpp@naver.com>
2020-07-21 17:13:02 +0900

    FEAT: Add commandline handling logic.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 mod compiler;
 mod runtime;
 
+use clap::{App, Arg};
 use compiler::lookahead_lexer::LookaheadLexer as Lexer;
 use compiler::parser::parse;
 use runtime::context::Context;
 use std::fs::read_to_string;
+use std::process::exit;
 
 fn main() {
-    let mut lexer = Lexer::new(read_to_string("examples/test.piped").unwrap());
+    let matches = App::new("piped")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A pipeline orchestrator for everyone to build softwares easier and faster.")
+        .arg(
+            Arg::with_name("input")
+                .help("A pipeline scripe file to execute")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input = matches.values_of("input").unwrap().last().unwrap();
+
+    let mut lexer = Lexer::new(match read_to_string(input) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Unable to open the given path: {}", input);
+            eprintln!("\tbecause: {}", err);
+            eprintln!("\texiting.");
+            exit(-1);
+        }
+    });
     let ast_vec = parse(&mut lexer);
     let mut context = Context::new();
 
